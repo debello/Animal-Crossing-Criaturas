974e0b56a292e9d1b7c7a7ea9a7cd00b91a5f6e4
debello <42223093+debello@users.noreply.github.com>
2019-08-06 00:46:55 +0200

    kinda-mobile responsive

--- newtable.php
+++ newtable.php
@@ -26,90 +26,95 @@ require_once 'arrayJSON.php';
 ?>
 <div>
 <div class="container limiter">
-	<p>Guía de Criaturas, Peinados... en Animal Crossing AC
-	<br>
-	Basada en la original de 
-	<a href="http://www.guiasnintendo.com/0a_NINTENDO_3DS/animal_crossing_new_leaf/animal_crossing_new_leaf_sp/peces.html">guiasnintendo</a>.</p>
+	<p>Guía de Criaturas, Peinados... en Animal Crossing AC</p>
+	<!-- <a href="http://www.guiasnintendo.com/0a_NINTENDO_3DS/animal_crossing_new_leaf/animal_crossing_new_leaf_sp/peces.html">guiasnintendo</a>.</p> -->
 		<!-- <a href="http://www.elnauta.net/animal-crossing-leaf-la-peluqueria-de-chez-marilin-elige-tu-peinado/"> -->
 	<button class="button button2" onclick="showTable()"> Lista de Bichos  </button>
 	<button class="button button1" onclick="showTable2()">Peinados</a></button>
 	<div id="imagen-peinados" style="display:none">
-		<img src="images/peinados_.png">
-		<img src="images/colores_.png">
+		<img src="images/peinados_.png" class="img-fluid">
+		<img src="images/colores_.png" class="img-fluid">
 	</div>
 	<div id="tablas-bichos" style="display:none">
-	<table class="table" >
-		<thead class="table-dark">
-			<tr>
-				<th>Imagen</th>
-				<th>Insecto</th>
-				<th>Bayas</th>
-				<th>Ubicacion</th>
-				<th>Meses</th>
-				<th>Horas</th>
-			</tr>
-		</thead>
+		<div class="table-responsive">
+			<table class="table w-auto" >
+				<thead class="table-dark">
+					<tr>
+						<th class="th-lg">Imagen</th>
+						<th class="th-lg">Insecto</th>
+						<th class="th-lg">Bayas</th>
+						<th class="th-lg">Ubicacion</th>
+						<th class="th-lg">Meses</th>
+						<th class="th-lg">Horas</th>
+					</tr>
+				</thead>
 
-		<?php for ($i = 0; $i<$sizeRowInsectos; $i++) {?>
-		<tbody class="table-dark">
-			<tr>
-				<td><img src='<?php echo $insectos["Imagen"][$i];?>' width="50px" /></td>
-				<td><?php echo $insectos["Insecto"][$i];?></td>
-				<td><?php echo $insectos["Bayas"][$i];?></td>
-				<td><?php echo $insectos["Ubicacion"][$i];?></td>
-				<td><?php echo $insectos["Meses"][$i];?></td>
-				<td><?php echo $insectos["Horas"][$i];}?></td>
-			</tr>
-		</tbody>
-	</table>
-	<table class="table" >
-		<thead class="table-dark">
-			<tr>
-				<th>Imagen</th>
-				<th>Pez</th>
-				<th>Bayas</th>
-				<th>Ubicacion</th>
-				<th>Meses</th>
-				<th>Horas</th>
-			</tr>
-		</thead>
+				<?php for ($i = 0; $i<$sizeRowInsectos; $i++) {?>
+				<tbody class="table-dark">
+					<tr>
+						<td><img src='<?php echo $insectos["Imagen"][$i];?>' width="50px" /></td>
+						<td><?php echo $insectos["Insecto"][$i];?></td>
+						<td><?php echo $insectos["Bayas"][$i];?></td>
+						<td><?php echo $insectos["Ubicacion"][$i];?></td>
+						<td><?php echo $insectos["Meses"][$i];?></td>
+						<td><?php echo $insectos["Horas"][$i];}?></td>
+					</tr>
+				</tbody>
+			</table>
+		<!-- </div> -->
+		<!-- <div class="table-responsive"> -->
+			<table class="table w-auto" >
+				<thead class="table-dark">
+					<tr>
+						<th class="th-lg">Imagen</th>
+						<th class="th-lg">Pez</th>
+						<th class="th-lg">Bayas</th>
+						<th class="th-lg">Ubicacion</th>
+						<th class="th-lg">Meses</th>
+						<th class="th-lg">Horas</th>
+					</tr>
+				</thead>
 
-		<?php for ($i = 0; $i<$sizeRowPeces; $i++) {?>
-		<tbody class="table-dark">
-			<tr>
-				<td><img src='<?php echo $peces["Imagen"][$i];?>' width="50px" /></td>
-				<td><?php echo $peces["Pez"][$i];?></td>
-				<td><?php echo $peces["Bayas"][$i];?></td>
-				<td><?php echo $peces["Ubicacion"][$i];?></td>
-				<td><?php echo $peces["Meses"][$i];?></td>
-				<td><?php echo $peces["Horas"][$i];}?></td>
-			</tr>
-		</tbody>
-	</table>
-	<table class="table" >
-		<thead class="table-dark">
-			<tr>
-				<th>Imagen</th>
-				<th>Animal Submarino</th>
-				<th>Bayas</th>
-				<th>Tamaño Sombra</th>
-				<th>Meses</th>
-				<th>Horas</th>
-			</tr>
-		</thead>
+				<?php for ($i = 0; $i<$sizeRowPeces; $i++) {?>
+				<tbody class="table-dark">
+					<tr>
+						<td><img src='<?php echo $peces["Imagen"][$i];?>' width="50px" /></td>
+						<td><?php echo $peces["Pez"][$i];?></td>
+						<td><?php echo $peces["Bayas"][$i];?></td>
+						<td><?php echo $peces["Ubicacion"][$i];?></td>
+						<td><?php echo $peces["Meses"][$i];?></td>
+						<td><?php echo $peces["Horas"][$i];}?></td>
+					</tr>
+				</tbody>
+			</table>
+		<!-- </div> -->
+		<!-- <div class="table-responsive"> -->
+			<table class="table w-auto" >
+				<thead class="table-dark">
+					<tr>
+						<th class="th-lg">Imagen</th>
+						<th class="th-lg">Animal Submarino</th>
+						<th class="th-lg">Bayas</th>
+						<th class="th-lg">Tamaño Sombra</th>
+						<th class="th-lg">Meses</th>
+						<th class="th-lg">Horas</th>
+					</tr>
+				</thead>
 
-		<?php for ($i = 0; $i<$sizeRowSubmarinos; $i++) {?>
-		<tbody class="table-dark">
-			<tr>
-				<td><img src='<?php echo $submarinos["Imagen"][$i];?>' width="50px" /></td>
-				<td><?php echo $submarinos["Animal-Submarino"][$i];?></td>
-				<td><?php echo $submarinos["Bayas"][$i];?></td>
-				<td><?php echo $submarinos["Tamaño-sombra"][$i];?></td>
-				<td><?php echo $submarinos["Meses"][$i];?></td>
-				<td><?php echo $submarinos["Horas"][$i];}?></td>
-			</tr>
-		</tbody>
-	</table>
+				<?php for ($i = 0; $i<$sizeRowSubmarinos; $i++) {?>
+				<tbody class="table-dark">
+					<tr>
+						<td><img src='<?php echo $submarinos["Imagen"][$i];?>' width="50px" /></td>
+						<td><?php echo $submarinos["Animal-Submarino"][$i];?></td>
+						<td><?php echo $submarinos["Bayas"][$i];?></td>
+						<td><?php echo $submarinos["Tamaño-sombra"][$i];?></td>
+						<td><?php echo $submarinos["Meses"][$i];?></td>
+						<td><?php echo $submarinos["Horas"][$i];}?></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
 
 <script type="text/javascript">
